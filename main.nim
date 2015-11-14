bb1915aa66f45ec090085be665a698ffbaf37526
c0ffee <c0ffeeartc@.gmail.com>
2015-11-14 15:16:59 +0200

    initial commit

---
+++ main.nim
@@ -0,0 +1,14 @@
+template SECTION(body:stmt):stmt =
+    body
+
+block:
+    var s:string = ""
+    SECTION:
+        s&="a"
+        SECTION:
+            s&="b"
+        SECTION:
+            s&="c"
+    echo s
+# assert(s=="abac")
+
